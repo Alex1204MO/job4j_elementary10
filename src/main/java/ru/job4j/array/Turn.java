b2b722ec35df98d530a4fbf52d9936f12c7f6b25
Aleksandr Purgin <aleksandr-purgin@inbox.ru>
2023-08-21 21:50:04 +0300

    Перевернуть массив. [#226790 #438253] 21

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -4,12 +4,10 @@ import java.util.Arrays;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int dlina = array.length;
-        for (int i = 0; i < dlina / 2; i++) {
-            int v = array[i];
-            int j = dlina - 1 - i;
-            array[i] = array[j];
-            array[j] = v;
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
         return array;
     }
